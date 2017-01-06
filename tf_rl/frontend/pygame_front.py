5f5e37b8fd241a374747572f0d0a11f6a977c691
feruxmax <feruxmax@gmail.com>
2017-01-07 00:47:34 +0300

    #1 begin adding pygame frontend: * create frontend module with existing svg and new pygame frontend * switch project to pygame frontend (not compatible with svg yet) * get karpathy_game.ipnb cod to run.py for debug

---
+++ tf_rl/frontend/pygame_front.py
@@ -0,0 +1,89 @@
+import sys
+import pygame
+
+YELLOW = 255, 255, 0
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255 
+BLUE = 0, 0, 255
+
+class Scene:
+
+    def __init__(self, size=(640, 480)):
+        pygame.init()
+        self.items = []
+        self.width = size[0]
+        self.high = size[1]
+        self.size = size
+        self.screen = pygame.display.set_mode(self.size)
+
+    def add(self, item):
+        self.items.append(item)
+
+    def clear(self):
+        self.items.clear()
+
+    def draw(self):
+        self.screen.fill(WHITE)
+        for item in self.items:
+            item.draw(self.screen) 
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        
+  
+class Circle:
+    def __init__(self,center,radius,color):
+        self.surf = pygame.Surface((2*radius, 2*radius))
+        self.surf.set_colorkey(WHITE)
+        self.surf.fill(WHITE)
+        self.rect = pygame.draw.circle(self.surf, color, (radius, radius), radius)
+        self.rect = self.rect.move(int(center[0]), int(center[1]))
+
+    def draw(self, screen):
+        screen.blit(self.surf, self.rect)
+
+def test():
+    pygame.init()
+    WORLD_SIZE = width, height = 1024, 768 
+    BALL_SIZE = 50
+
+    speed = [1, 1]
+    
+    screen = pygame.display.set_mode(WORLD_SIZE)
+    
+    ball = pygame.Surface((BALL_SIZE, BALL_SIZE))
+    ballrect = pygame.draw.circle(ball, BLUE, (BALL_SIZE//2, BALL_SIZE//2), BALL_SIZE//2)
+    
+    #for j in range(1000):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+    
+        ballrect = ballrect.move(speed)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+    
+        screen.fill(BLACK)
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+def test2():
+    sceene = Scene((640, 480))
+    sceene.add(Circle((40,50),10))
+    sceene.add(Circle((140,50),10))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        sceene.draw()
+
+if __name__ == '__main__':
+    test()
