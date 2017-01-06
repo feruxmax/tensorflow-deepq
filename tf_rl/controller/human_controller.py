5ac1427c8728309da74b3e4d6762493f7cbd62be
feruxmax <feruxmax@gmail.com>
2017-01-07 02:09:04 +0300

    #1 process double pressed keys

--- tf_rl/controller/human_controller.py
+++ tf_rl/controller/human_controller.py
@@ -2,15 +2,23 @@ class HumanController(object):
     def __init__(self, frontend):
         self.frontend = frontend 
         self.experience = []
+        self.last_action = None
 
     def action(self, o):
         action_codes = {"w":3, "a":2, "s":1, "d":0, None: 4}
         wasd = self.frontend.get_keys()
         action = None
-        if len(wasd) > 0:
-            action = wasd[-1]
-        else:
+        if len(wasd) == 0:
             action = None 
+        elif len(wasd) == 1:
+            action = wasd[-1]
+        elif len(wasd) == 2:
+            if wasd[-1] != self.last_action:
+                action = wasd[-1]
+            else:
+                action = wasd[0]
+
+        self.last_action = action
 
         return action_codes[action]
 
